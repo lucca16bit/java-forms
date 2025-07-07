8f38d05dc8a91e5eb539a70c0457429ce0885721
lucas <lucaston2323@gmail.com>
2025-07-06 21:26:46 -0300

    First Commit

---
+++ src/test/java/br/com/lucca16bit/forms/FormsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucca16bit.forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
